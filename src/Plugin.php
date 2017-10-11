27af99736809eefa377eac080f8077c1e123616f
Lucas Weijers <lucas@dolphiq.nl>
2017-10-11 15:19:29 +0200

    enable cp settings

--- src/Plugin.php
+++ src/Plugin.php
@@ -10,18 +10,36 @@
 namespace plugins\dolphiq\form;
 
 use Craft;
+use plugins\dolphiq\form\controllers\MainController;
+use plugins\dolphiq\form\models\Settings;
 use plugins\dolphiq\form\twigextensions\YiiTwigExtension;
 
 class Plugin extends \craft\base\Plugin
 {
-  public function init()
-  {
-    parent::init();
+    public $hasCpSettings = true;
 
-    // Custom initialization code goes here...
-    Craft::$app->view->twig->addExtension(new YiiTwigExtension());
+    public function init()
+    {
+        parent::init();
 
-  }
+        // Custom initialization code goes here...
+        Craft::$app->view->twig->addExtension(new YiiTwigExtension());
+    }
+
+    protected function createSettingsModel()
+    {
+        return new Settings();
+    }
+
+    protected function settingsHtml()
+    {
+        $mainController = new MainController('dq-forms-main', 'dolphiq-form');
+
+        return \Craft::$app->getView()->renderTemplate('dolphiq-form/settings', [
+            'forms' => $mainController->getForms(),
+            'settings' => $this->getSettings()
+        ]);
+    }
 }
 
 ?>
