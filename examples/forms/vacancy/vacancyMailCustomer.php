cc2ede6b63ed64cc7c6749ab1d174f8dbe47c8a1
Lucas Weijers <lucas@dolphiq.nl>
2017-10-16 15:27:40 +0200

    add default vacancy form

---
+++ examples/forms/vacancy/vacancyMailCustomer.php
@@ -0,0 +1,11 @@
+<?php
+
+/* @var $this \yii\web\View view component instance */
+/* @var $message \yii\mail\BaseMessage instance of newly created mail message */
+/* @var $model app\forms\vacancyForm */
+
+?>
+<h2>Thank you for your message <?= $model->lastname; ?></h2>
+<p>
+  We will contact you as soon as possible
+</p>
