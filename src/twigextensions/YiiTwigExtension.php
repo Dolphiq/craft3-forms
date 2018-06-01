336188c2f67bfefdd381b154144fd96718449257
Lucas Weijers <lucas@dolphiq.nl>
2018-06-01 14:24:30 +0200

    Use class instead of className for use in recent php versions

--- src/twigextensions/YiiTwigExtension.php
+++ src/twigextensions/YiiTwigExtension.php
@@ -50,10 +50,9 @@ $.ajax({
 JS;
 
         Craft::$app->view->registerJs($script);
-        Craft::$app->view->registerAssetBundle(pjaxAsset::className());
-        Craft::$app->view->registerAssetBundle(ValidationAsset::className());
-        Craft::$app->view->registerAssetBundle(ActiveFormAsset::className());
-
+        Craft::$app->view->registerAssetBundle(pjaxAsset::class);
+        Craft::$app->view->registerAssetBundle(ValidationAsset::class);
+        Craft::$app->view->registerAssetBundle(ActiveFormAsset::class);
         return TemplateHelper::raw('<div id="dolphiqFormPlugin-'.$uniqId.'" data-ajaxfill="'.UrlHelper::actionUrl('dolphiq-craft3-forms/main/index', ['handle' => $handle, 'params' => json_encode($params)]).'" class="form--wrapper"></div>');
     }
 
